d58169a09d527e5ce7930a6361dce3f588ab97f2
shenh10 <thushenhan@gmail.com>
2016-12-13 03:23:23 +0800

    initial working kernel-based object tracking

---
+++ sensors/object_tracking/main.m
@@ -0,0 +1,26 @@
+input = '../../../cv/tracker_benchmarks/data/otb100/Human2/img';
+ext = 'jpg';
+D = dir(fullfile(input,['*.', ext]));
+file_list={D.name};
+start_frame = 1;
+end_frame = numel(file_list);
+groundtruth = fullfile(input,'../groundtruth_rect.txt');
+total_gt = textread(groundtruth,'','delimiter',',');
+init_rect = total_gt(1,:);
+rect = init_rect;
+m = 20;
+
+for frame_id = start_frame:end_frame
+figure(1);
+I_orig=imread(fullfile(input,file_list{frame_id}));
+imshow(I_orig);
+if frame_id == start_frame
+    I_crop =imcrop(I_orig,init_rect);
+    [target_q,~] = color_distro(I_crop, m, 1);
+    continue;
+end
+
+rect = kernel_ot( I_orig, target_q, m, rect );
+rectangle('position',rect,'LineWidth',2,'EdgeColor','r');
+pause(0.01);
+end
